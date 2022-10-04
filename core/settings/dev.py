1d824056023af08fd51a3a4fe0b4eb6afdf95aba
Philip Lebios <flipdruid@yahoo.com>
2022-10-04 22:44:58 +0800

    adding local-bootstrap-jquery2

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -24,3 +24,9 @@ else:
             "PORT": os.environ.get("PG_PORT"),
         }
     }
+
+CELERY_BROKER_URL           = 'redis://127.0.0.1:6379'
+CELERY_ACCEPT_CONTENT       = ['application/json']
+CELERY_RESULT_SERIALIZER    = 'json'
+CELERY_TASK_SERIALIZER      = 'json'
+CELERY_TIMEZONE             = 'Asia/Manila'
