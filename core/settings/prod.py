d7ff13e888967283d8d2472ddd2765fc8d96c736
flipdruid <flipdruid@yahoo.com>
2022-11-21 23:42:00 +0800

    feature/user-auth in heroku

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -7,9 +7,24 @@ from decouple import config
 
 from dotenv import load_dotenv, find_dotenv
 
-ALLOWED_HOSTS = ["logicmorphdevs.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["logicmorph.herokuapp.com", "127.0.0.1"]
 
 SECRET_KEY=config("SECRET_KEY")
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_USE_TLS = config("EMAIL_USE_SSL", True)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+DEFAULT_FROM_MAIL = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
+# UserAuth
+AUTH_USER_MODEL = "accounts.User"
+LOGIN_URL = config("LOGIN_URL")
+LOGOUT_REDIRECT_URL = config("LOGOUT_REDIRECT_URL")
+LOGIN_REDIRECT_URL = config("LOGIN_REDIRECT_URL")
+
+# SALT
+HASHID_FIELD_SALT = config("HASHID_FIELD_SALT")
 
 DEBUG = False
 
@@ -21,10 +36,6 @@ DATABASES = {
     "default": dj_database_url.config(default='sqlite://db.sqlite3', 
     conn_max_age=600, ssl_require=False)
 }
-
-
-
-
 
 LOGGING = {
     'version': 1,
