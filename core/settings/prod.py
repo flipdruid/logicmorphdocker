f60926908f7260675b014085141ad4192b198ea1
flipdruid <flipdruid@yahoo.com>
2022-11-27 23:41:25 +0800

    migrate

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -32,7 +32,6 @@ DEBUG = False
 load_dotenv(find_dotenv())
 
 import dj_database_url
-
 DATABASES = {
     "default": dj_database_url.config(default='sqlite://db.sqlite3', 
     conn_max_age=600, ssl_require=False)
