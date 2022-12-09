ccfcb5c211ddded7dc289ed8efb551002d96af32
flipdruid <flipdruid@yahoo.com>
2022-12-09 09:59:53 +0800

    darkmode theme5

--- core/settings/base.py
+++ core/settings/base.py
@@ -211,7 +211,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 #   STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 #Uncomment if upload heroku
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] #disable if upload to heroku
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] #disable if upload to heroku
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
