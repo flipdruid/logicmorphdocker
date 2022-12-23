9a17ffbd2f19bc92ffdb11632112721acd0b1261
flipdruid <flipdruid@yahoo.com>
2022-12-23 21:39:57 +0800

    new landing page design for checking

--- app/views/index.py
+++ app/views/index.py
@@ -4,8 +4,9 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from accounts.decorators import allowed_users
-
+from django.core.exceptions import ObjectDoesNotExist
 from app.models import Post
+from app.models.profile import Profile
 
 
 class Error400(TemplateView):
@@ -29,6 +30,30 @@ class Landing(ListView):
     template_name = "main/landing.html"
     paginate_by = 9
     context_object_name = "posts"
+
+    def get_context_data(self, **kwargs):
+        context     =    super(Landing, self).get_context_data(**kwargs)
+        posts       =    Post.objects.all()
+        post_lists  ={}
+        profile_img=""
+
+        for post in posts:
+            try:
+                profile_img = Profile.objects.get(user=post.created_by)
+                post_lists[post.id] = {
+                    "title" : post.title,
+                    "description": post.description,
+                    "created_by":post.created_by,
+                    "created_at":post.created_at,
+                    "updated_at":post.updated_at,
+                    "profile_img": profile_img.avatar
+                }
+
+            except ObjectDoesNotExist:  
+                pass
+            context['posts']     =    post_lists      
+        # context['show_convert']     =  User.objects.filter(email=currentObject.email)
+        return context
 
 class Carousel(ListView):
     model = Post
